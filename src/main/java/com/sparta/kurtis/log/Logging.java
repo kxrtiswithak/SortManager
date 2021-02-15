cfbd85f4e471a30fb2fc9dad877fcb7499c621bf
kxrtiswithak <kxrtiswithak@gmail.com>
2021-02-15 02:24:14 +0000

    add logging dependency

---
+++ src/main/java/com/sparta/kurtis/log/Logging.java
@@ -0,0 +1,9 @@
+package com.sparta.kurtis.log;
+
+import org.apache.logging.log4j.LogManager;
+
+public class Logger {
+    private static org.apache.logging.log4j.Logger logger = LogManager.getLogger(Logging.class);
+
+
+}
