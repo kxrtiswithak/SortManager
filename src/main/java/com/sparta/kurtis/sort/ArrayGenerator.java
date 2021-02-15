abe70ec8d5d36645fb6b0403de20c00cc3917b01
kxrtiswithak <kxrtiswithak@gmail.com>
2021-02-15 01:53:58 +0000

    add array generator

--- src/main/java/com/sparta/kurtis/sort/ArrayGenerator.java
+++ src/main/java/com/sparta/kurtis/sort/ArrayGenerator.java
@@ -1,2 +1,19 @@
-package com.sparta.kurtis.sort;public class ArrayGenerator {
+package com.sparta.kurtis.sort;
+
+import java.util.Random;
+
+public class ArrayGenerator {
+
+    public static int[] generateIntArray(int length) {
+        return generateIntArray(length, 1, 100);
+    }
+
+    public static int[] generateIntArray(int length, int lowest, int highest) {
+        int[] generatedArray = new int[length];
+
+        for (int i = 0; i < generatedArray.length; i++) {
+            generatedArray[i] = new Random().nextInt(highest - lowest + 1) + lowest;
+        }
+        return generatedArray;
+    }
 }
