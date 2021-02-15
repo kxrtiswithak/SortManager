efa417a74f41df9a76779bbb1509ef6ad78df3bd
kxrtiswithak <kxrtiswithak@gmail.com>
2021-02-15 02:03:26 +0000

    refactor starter, add binary examples

--- src/main/java/com/sparta/kurtis/start/Starter.java
+++ src/main/java/com/sparta/kurtis/start/Starter.java
@@ -1,70 +1,67 @@
 package com.sparta.kurtis.start;
 
-import com.sparta.kurtis.sorters.BubbleSort;
-import com.sparta.kurtis.sorters.MergeSort;
-import com.sparta.kurtis.view.Printer;
+import com.sparta.kurtis.sort.*;
+import com.sparta.kurtis.print.Printer;
+
+import java.util.Random;
 
 public class Starter {
+    private static SortFactory sortFactory = new SortFactory();
     public static void start() {
+        // bubbleSortExamples();
+        // mergeSortExamples();
+        binaryTreeSortExamples();
 
+    }
+
+    public static void bubbleSortExamples() {
         Printer.printString("\n\nBubble Sort Examples\n-----------------------------------------");
-        BubbleSort bubbleSort = new BubbleSort();
-        int[] unsortedArray1 = new int[]{5, 3, 8, 1, 10, -7, -6, -5, -2, 2, 7, 6, 4, 9};
+        Sorter bubbleSort = sortFactory.getSort("bubble");
 
+        int[] unsortedArray1 = ArrayGenerator.generateIntArray(20, -100, 100);
         Printer.printString("\nBubble sorting unsorted array. before: ");
         Printer.printIntArray(unsortedArray1);
         Printer.printString("after: ");
-        Printer.printIntArray(bubbleSort.sortArray(unsortedArray1));
-
-        int[] sortedArray1 = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        unsortedArray1 = bubbleSort.sortArray(unsortedArray1);
+        Printer.printIntArray(unsortedArray1);
 
         Printer.printString("\nBubble sorting sorted array. before: ");
-        Printer.printIntArray(sortedArray1);
+        Printer.printIntArray(unsortedArray1);
         Printer.printString("after: ");
-        Printer.printIntArray(bubbleSort.sortArray(sortedArray1));
+        unsortedArray1 = bubbleSort.sortArray(unsortedArray1);
+        Printer.printIntArray(unsortedArray1);
+    }
 
+    public static void mergeSortExamples() {
         Printer.printString("\n\nMerge Sort Examples\n-----------------------------------------");
-        MergeSort mergeSort = new MergeSort();
-        int[] unsortedArray2 = new int[]{/*90, 23, 101, 45, 65, 23, 67, 89, 34, 23*/};
+        Sorter mergeSort = sortFactory.getSort("merge");
+        int[] unsortedArray2 = ArrayGenerator.generateIntArray(10, -200, 200);
 
         Printer.printString("\nMerge sorting unsorted array. before: ");
         Printer.printIntArray(unsortedArray2);
         Printer.printString("after: ");
-        Printer.printIntArray((mergeSort.sortArray(unsortedArray2)));
+        unsortedArray2 = mergeSort.sortArray(unsortedArray2);
+        Printer.printIntArray(unsortedArray2);
 
         Printer.printString("\nMerge sorting sorted array. before: ");
-        Printer.printIntArray(sortedArray1);
-        Printer.printString("after: ");
-        Printer.printIntArray((mergeSort.sortArray(sortedArray1)));
-
-        unsortedArray1 = new int[]{5, 3, 8, 1, 10, -7, -6, -5, -2, 2, 7, 6, 4, 9};
-        unsortedArray2 = new int[]{90, 23, 101, 45, 65, 23, 67, 89, 34, 23};
-
-        Printer.printString("\nMerge sorting two unsorted arrays. before: ");
-        Printer.printIntArray(unsortedArray1);
         Printer.printIntArray(unsortedArray2);
         Printer.printString("after: ");
-        Printer.printIntArray(mergeSort.sortTwoArrays(unsortedArray1, unsortedArray2));
+        unsortedArray2 = mergeSort.sortArray(unsortedArray2);
+        Printer.printIntArray(unsortedArray2);
+    }
 
-        int[] sortedArray2 = new int[]{2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
-        int[] sortedArray3 = new int[]{3, 7, 11, 19, 42, 76, 141, 983, 1003, 2500};
+    public static void binaryTreeSortExamples() {
+        Printer.printString("\n\nBinary Tree Sort Examples\n-----------------------------------------");
+        BinaryTree binaryTreeSort = sortFactory.getTreeSort("binary");
 
-        Printer.printString("\nMerging two sorted arrays. before: ");
-        Printer.printIntArray(sortedArray2);
-        Printer.printIntArray(sortedArray3);
-        Printer.printString("after: ");
-        Printer.printIntArray(mergeSort.mergeTwoSortedArrays(sortedArray2, sortedArray3));
+        int[] testArray = ArrayGenerator.generateIntArray(10);
+        Printer.printIntArray(testArray);
+        binaryTreeSort.addElements(testArray);
 
-        int[] unsortedArray4 = new int[]{1, 5, 2, 7, 3, 8, 3,  90, 32, 7, 12, 98, -16, 777};
-        int[] unsortedArray5 = new int[]{1, 5, 2, 7, 3, 8, 3,  90, 32, 7, 12, 98, -16, 777};
-        Printer.printString("\nMerging two unsorted arrays. before: ");
-        Printer.printIntArray(unsortedArray4);
-        Printer.printIntArray(unsortedArray5);
-        Printer.printString("after: ");
-        Printer.printIntArray(mergeSort.mergeTwoSortedArrays(unsortedArray4, unsortedArray5));
+        Printer.printIntArray(binaryTreeSort.getSortedTreeAsc());
 
-        Printer.printString();
+        binaryTreeSort.addElement(5);
 
-
+        Printer.printIntArray(binaryTreeSort.getSortedTreeAsc());
     }
 }
