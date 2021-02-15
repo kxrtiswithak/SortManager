ec7455c5dec53861e8391c3c115fcee1191602fa
kxrtiswithak <kxrtiswithak@gmail.com>
2021-02-15 01:54:56 +0000

    add factory pattern

--- src/main/java/com/sparta/kurtis/sort/SortFactory.java
+++ src/main/java/com/sparta/kurtis/sort/SortFactory.java
@@ -1,2 +1,25 @@
-package com.sparta.kurtis.sort;public class SortFactory {
+package com.sparta.kurtis.sort;
+
+public class SortFactory {
+    public Sorter getSort(String sortType) {
+
+        switch(sortType.toLowerCase()) {
+            case "bubble":
+                return new BubbleSort();
+            case "merge":
+                return new MergeSort();
+            default:
+                return null;
+        }
+
+    }
+
+    public BinaryTree getTreeSort(String sortType) {
+        switch(sortType.toLowerCase()) {
+            case "binary":
+                return new BinaryTreeSort();
+            default:
+                return null;
+        }
+    }
 }
